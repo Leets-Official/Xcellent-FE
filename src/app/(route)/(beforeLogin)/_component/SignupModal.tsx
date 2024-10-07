6aa9126fdf9a8a23f8e894cd6012fd394d7ff386
dahhyeon <dahhyeon22@gmail.com>
2024-10-07 20:07:06 +0900

    feat:회원가입 폼 ui

--- src/app/(route)/(beforeLogin)/_component/SignupModal.tsx
+++ src/app/(route)/(beforeLogin)/_component/SignupModal.tsx
@@ -1,3 +1,12 @@
 export default function SignupModal() {
-  return <div>회원가입 모달</div>;
+  return (
+    <div>
+      <form method="POST" action="/api/auth/signup">
+        <input name="name" type="text" placeholder="이름" />
+        <input name="email" type="text" placeholder="이메일" />
+        <input name="password" type="password" placeholder="비번" />
+        <button type="submit">가입하기</button>
+      </form>
+    </div>
+  );
 }
