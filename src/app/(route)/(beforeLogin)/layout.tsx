fda68f99bead88bc8db05c570cf71635759c77a4
dahhyeon <dahhyeon22@gmail.com>
2024-11-03 19:39:59 +0900

    feat:닫기 버튼 클릭 시 모달 창이 사라지도록 추가

--- src/app/(route)/(beforeLogin)/layout.tsx
+++ src/app/(route)/(beforeLogin)/layout.tsx
@@ -4,7 +4,6 @@ type Props = { children: ReactNode; modal: ReactNode };
 export default function BeforeLoginLayout({ children, modal }: Props) {
   return (
     <div>
-      before login 레이아웃
       {children}
       {modal}
     </div>
--- src/app/(route)/(beforeLogin)/login/_component/RedirectToLogin.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-import Main from '@/app/(route)/(beforeLogin)/_component/Main';
-import { useRouter } from 'next/navigation';
-
-export default function RedirectToLogin() {
-  const router = useRouter();
-  router.replace('/i/flow/login');
-  return <Main />;
-}
