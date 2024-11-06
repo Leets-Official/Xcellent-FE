63c09fba0f0369fb7b09f7b1e33cef2b955dff6f
dahhyeon <dahhyeon22@gmail.com>
2024-11-06 22:40:24 +0900

    style:로그인모달 ui 수정

--- src/app/(route)/(beforeLogin)/_component/LoginModal.tsx
+++ src/app/(route)/(beforeLogin)/_component/LoginModal.tsx
@@ -1,3 +1,4 @@
+// 로그인 모달 컴포넌트
 'use client';
 
 import { useState } from 'react';
@@ -34,7 +35,6 @@ export default function LoginModal() {
       const res = await Login(formData);
       if (!res.ok) {
         const errorData = await res.json();
-        console.log('ErrorData: ', errorData);
         if (errorData.code === 401) {
           setMessage('이메일과 비밀번호가 일치하지 않습니다.');
         } else if (errorData.code === 404) {
@@ -42,7 +42,6 @@ export default function LoginModal() {
         } else {
           setMessage('로그인 중 오류가 발생하였습니다.');
         }
-        console.log(formData);
       } else {
         const data = await res.json();
 
@@ -50,7 +49,7 @@ export default function LoginModal() {
         localStorage.setItem('refreshToken', data.result.refreshToken);
 
         alert('로그인에 성공했습니다.');
-        router.replace('/home'); // 로그인 성공 시 홈으로 이동
+        router.replace('/home');
       }
     } catch (err) {
       console.error(err);
@@ -59,14 +58,15 @@ export default function LoginModal() {
   };
 
   return (
-    <div
-      className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-60 backdrop-blur-sm"
-      style={{ backgroundColor: 'rgba(104, 132, 145, 0.413)' }}
-    >
-      <div className="bg-black p-6 rounded-lg max-w-sm w-full text-center">
-        <h2 className="text-2xl font-semibold text-white mb-4">로그인하세요</h2>
-
-        <form onSubmit={onSubmit} className="space-y-4">
+    isOpen && (
+      <div
+        className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-60 backdrop-blur-sm"
+        style={{ backgroundColor: 'rgba(104, 132, 145, 0.413)' }}
+      >
+        <form
+          onSubmit={onSubmit}
+          className="bg-black rounded-xl p-6 w-full max-w-md sm:max-w-lg md:max-w-xl lg:max-w-xl"
+        >
           <button
             onClick={handleClose}
             className="hover:bg-gray-700 rounded-full w-8 h-8 flex items-center justify-center"
@@ -76,53 +76,49 @@ export default function LoginModal() {
               height="20"
               viewBox="0 0 24 24"
               aria-hidden="true"
-              className="fill-white r-4qtqp9 r-yyyyoo r-dnmrzs r-bnwqim r-lrvibr r-m6rgpd r-z80fyv r-19wmn03"
+              className="fill-white"
             >
               <g>
                 <path d="M10.59 12L4.54 5.96l1.42-1.42L12 10.59l6.04-6.05 1.42 1.42L13.41 12l6.05 6.04-1.42 1.42L12 13.41l-6.04 6.05-1.42-1.42L10.59 12z" />
               </g>
             </svg>
           </button>
-          <div>
-            <label htmlFor="email" className="block text-white mb-1">
-              이메일
-            </label>
-            <input
-              id="email"
-              name="email"
-              className="w-full px-3 py-2 bg-gray-700 rounded-lg text-white"
-              value={formData.email}
-              onChange={handleChange}
-              type="text"
-              placeholder="이메일"
-            />
+          <div className="flex flex-col justify-center items-center gap-5">
+            <p className="text-white text-3xl font-bold">로그인하세요</p>
+            <div className="space-y-2">
+              <input
+                id="email"
+                name="email"
+                type="text"
+                value={formData.email}
+                onChange={handleChange}
+                placeholder="이메일"
+                required
+                className="w-80 p-4 bg-transparent text-white border border-gray-500 rounded-md focus:outline-none focus:border-3 focus:border-sky-400"
+              />
+            </div>
+            <div className="space-y-2">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                value={formData.password}
+                onChange={handleChange}
+                placeholder="비밀번호"
+                required
+                className="w-80 p-4 bg-transparent text-white border border-gray-500 rounded-md focus:outline-none focus:border-3 focus:border-sky-400"
+              />
+            </div>
+            {message && <p className="text-red-500 text-sm">{message}</p>}
+            <button
+              type="submit"
+              className="w-1/2 p-4 bg-white text-black rounded-full hover:bg-zinc-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+            >
+              로그인하기
+            </button>
           </div>
-
-          <div>
-            <label htmlFor="password" className="block text-white mb-1">
-              비밀번호
-            </label>
-            <input
-              id="password"
-              name="password"
-              className="w-full px-3 py-2 bg-gray-700 rounded-lg text-white"
-              value={formData.password}
-              onChange={handleChange}
-              type="password"
-              placeholder="비밀번호"
-            />
-          </div>
-
-          {message && <p className="text-red-500 text-sm">{message}</p>}
-
-          <button
-            type="submit"
-            className="w-1/2 p-4 bg-white text-black rounded-full hover:bg-zinc-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
-          >
-            로그인하기
-          </button>
         </form>
       </div>
-    </div>
+    )
   );
 }
