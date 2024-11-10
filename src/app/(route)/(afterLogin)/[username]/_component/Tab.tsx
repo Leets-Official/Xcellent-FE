180ea20698725b19fe8b90cb670e4f3ece3311ed
dahhyeon <dahhyeon22@gmail.com>
2024-11-10 17:13:31 +0900

    style:탭 hover 시 전체 영역의 배경색이 변하도록 수정

--- src/app/(route)/(afterLogin)/[username]/_component/Tab.tsx
+++ src/app/(route)/(afterLogin)/[username]/_component/Tab.tsx
@@ -38,30 +38,34 @@ export default function Tab({ type, userName }: TabProps) {
       <div className="flex justify-around w-full bg-transparent">
         {type === 'postsLikes' && (
           <>
-            <button
-              type="button"
-              onClick={onClickPost}
-              className={`px-4 py-2 ${
-                tab === 'post' ? 'text-white font-bold' : 'text-gray-500'
-              } relative hover:bg-gray-800`}
-            >
-              Posts
-              {tab === 'post' && (
-                <span className="absolute bottom-0 left-0 w-full h-1 bg-sky-500 rounded-full" />
-              )}
-            </button>
-            <button
-              type="button"
-              onClick={onClickLike}
-              className={`px-4 py-2 ${
-                tab === 'like' ? 'text-white font-bold' : 'text-gray-500'
-              } relative hover:bg-gray-800`}
-            >
-              Likes
-              {tab === 'like' && (
-                <span className="absolute bottom-0 left-0 w-full h-1 bg-sky-500 rounded-full" />
-              )}
-            </button>
+            <div className="text-center hover:bg-slate-800 w-1/2">
+              <button
+                type="button"
+                onClick={onClickPost}
+                className={`px-4 py-2 ${
+                  tab === 'post' ? 'text-white font-bold' : 'text-gray-500'
+                } relative hover:bg-gray-800`}
+              >
+                Posts
+                {tab === 'post' && (
+                  <span className="absolute bottom-0 left-0 w-full h-1 bg-sky-500 rounded-full" />
+                )}
+              </button>
+            </div>
+            <div className="text-center hover:bg-slate-800 w-1/2">
+              <button
+                type="button"
+                onClick={onClickLike}
+                className={`px-4 py-2 ${
+                  tab === 'like' ? 'text-white font-bold' : 'text-gray-500'
+                } relative hover:bg-gray-800`}
+              >
+                Likes
+                {tab === 'like' && (
+                  <span className="absolute bottom-0 left-0 w-full h-1 bg-sky-500 rounded-full" />
+                )}
+              </button>
+            </div>
           </>
         )}
         {type === 'followersFollowing' && (
