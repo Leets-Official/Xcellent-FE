8dfe4e9a29ed3d7cae0e93deab64892a8da4a03a
dahhyeon <dahhyeon22@gmail.com>
2024-11-10 22:29:17 +0900

    style:following button css 수정

--- src/app/(route)/(afterLogin)/[username]/_component/FollowingButton.tsx
+++ src/app/(route)/(afterLogin)/[username]/_component/FollowingButton.tsx
@@ -10,11 +10,11 @@ export default function FollowingButton() {
       type="button"
       onMouseEnter={() => setHover(true)}
       onMouseLeave={() => setHover(false)}
-      className={`text-white text-sm  font-bold p-1 rounded-full border border-white w-28 ml-auto
+      className={` text-sm  font-bold p-1 rounded-full w-28 ml-auto
         ${
           hover
-            ? 'bg-red-950 text-red-700 border border-red-700'
-            : 'text-whte border border-white'
+            ? ' bg-red-800 text-red-700 border border-red-700 bg-opacity-35'
+            : 'text-white border border-white'
         }`}
     >
       {hover ? 'Unfollow' : 'Following'}
