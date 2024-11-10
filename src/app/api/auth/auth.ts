bd23a9ddb04927cd45a3263ebf96fa68c84512e7
dahhyeon <dahhyeon22@gmail.com>
2024-11-10 23:27:13 +0900

    feat:profile info get 요청

--- src/app/api/auth/auth.ts
+++ src/app/api/auth/auth.ts
@@ -14,6 +14,21 @@ export interface LoginData {
   password: string;
 }
 
+export interface ProfileData {
+  email: string;
+  customId: string;
+  userName: string;
+  phoneNumber: string;
+  userBirthYear: number;
+  userBirthMonth: number;
+  userBirthDay: number;
+  profileImageUrl: string | null;
+  backgroundProfileImageUrl: string | null;
+  description: string | null;
+  websiteUrl: string | null;
+  location: string | null;
+}
+
 export const SignUp = async (data: SignUpData): Promise<any> => {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/register`,
@@ -42,4 +57,19 @@ export const Login = async (data: LoginData): Promise<any> => {
   );
 
   return res;
+};
+
+export const getProfileInfo = async (): Promise<ProfileData> => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/profile/myinfo`,
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    },
+  );
+
+  const data = await res.json();
+  return data.result;
 };
