6d23ee2dfd44e770f9533754103edab19b750216
dahhyeon <dahhyeon22@gmail.com>
2024-11-12 02:09:18 +0900

    fix: 프로필 클릭 시 해당 유저의 customId 경로로 이동

--- src/app/(route)/(afterLogin)/_component/Sidebar.tsx
+++ src/app/(route)/(afterLogin)/_component/Sidebar.tsx
@@ -1,38 +1,52 @@
 'use client';
 
+import { useEffect, useState } from 'react';
 import { useSelectedLayoutSegment } from 'next/navigation';
 import Link from 'next/link';
+import { getProfileInfo } from '@/app/api/user/user';
 
 export default function SideBar() {
   const segment = useSelectedLayoutSegment();
-  const me = {
-    id: 'dahyeon',
-  };
+  const [me, setMe] = useState<{ id: string } | null>(null);
+
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      try {
+        const userInfo = await getProfileInfo();
+        setMe({ id: userInfo.customId }); // customId로 설정
+      } catch (error) {
+        console.error('Failed to fetch user info: ', error);
+      }
+    };
+
+    fetchUserInfo();
+  }, []);
 
   const menuItems = [
     { href: '/home', label: 'Home', segmentKey: 'home' },
     { href: '/messages', label: 'Messages', segmentKey: 'messages' },
-    { href: `/${me.id}`, label: 'Profile', segmentKey: me.id },
+    { href: `/${me?.id}`, label: 'Profile', segmentKey: me?.id },
   ];
 
   return (
     <nav className="flex-1">
       <ul className="space-y-4">
-        {menuItems.map(({ href, label, segmentKey }) => (
-          <li key={href}>
-            <Link href={href}>
-              <div
-                className={`flex items-center gap-4 text-xl px-4 py-3 rounded-full ${
-                  segment === segmentKey
-                    ? 'font-extrabold'
-                    : 'hover:bg-gray-800'
-                }`}
-              >
-                <span>{label}</span>
-              </div>
-            </Link>
-          </li>
-        ))}
+        {me &&
+          menuItems.map(({ href, label, segmentKey }) => (
+            <li key={href}>
+              <Link href={href}>
+                <div
+                  className={`flex items-center gap-4 text-xl px-4 py-3 rounded-full ${
+                    segment === segmentKey
+                      ? 'font-extrabold'
+                      : 'hover:bg-gray-800'
+                  }`}
+                >
+                  <span>{label}</span>
+                </div>
+              </Link>
+            </li>
+          ))}
       </ul>
     </nav>
   );
