00180d346889029aff837cc522acec0741a4df09
dahhyeon <dahhyeon22@gmail.com>
2024-11-11 15:07:17 +0900

    refactor:Image를 가져오기 위해 nextconfig.mjs 파일 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['localhost'],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/app/(route)/(afterLogin)/_component/BackButton.tsx
+++ src/app/(route)/(afterLogin)/_component/BackButton.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
 export default function BackButton() {
@@ -7,18 +8,17 @@ export default function BackButton() {
 
   return (
     <button
+      type="button"
       onClick={() => router.back()}
       className="hover:bg-gray-700 rounded-full w-8 h-8 flex items-center justify-center"
     >
-      <svg
-        viewBox="0 0 24 24"
-        aria-hidden="true"
-        className="fill-white r-4qtqp9 r-yyyyoo r-dnmrzs r-bnwqim r-lrvibr r-m6rgpd r-z80fyv r-19wmn03 r-1otekoa"
-      >
-        <g>
-          <path d="M7.414 13l5.043 5.04-1.414 1.42L3.586 12l7.457-7.46 1.414 1.42L7.414 11H21v2H7.414z" />
-        </g>
-      </svg>
+      <Image
+        src="/backButton.svg"
+        alt="Back"
+        unoptimized
+        width={24}
+        height={24}
+      />
     </button>
   );
 }
