965146c2e0220157a823a196ddf833cdade9d750
dahhyeon <dahhyeon22@gmail.com>
2024-11-12 10:07:49 +0900

    refactor:ul,li태그 제거

--- src/app/(route)/(afterLogin)/_component/Sidebar.tsx
+++ src/app/(route)/(afterLogin)/_component/Sidebar.tsx
@@ -29,25 +29,18 @@ export default function SideBar() {
   ];
 
   return (
-    <nav className="flex-1">
-      <ul className="space-y-4">
-        {me &&
-          menuItems.map(({ href, label, segmentKey }) => (
-            <li key={href}>
-              <Link href={href}>
-                <div
-                  className={`flex items-center gap-4 text-xl px-4 py-3 rounded-full ${
-                    segment === segmentKey
-                      ? 'font-extrabold'
-                      : 'hover:bg-gray-800'
-                  }`}
-                >
-                  <span>{label}</span>
-                </div>
-              </Link>
-            </li>
-          ))}
-      </ul>
-    </nav>
+    <div className="space-y-4">
+      {menuItems.map(({ href, label, segmentKey }) => (
+        <div key={href}>
+          <Link href={href}>
+            <div
+              className={`flex items-center gap-4 text-xl px-4 py-3 rounded-full ${segment === segmentKey ? 'font-extrabold' : 'hover:bg-gray-800'}`}
+            >
+              <span>{label}</span>
+            </div>
+          </Link>
+        </div>
+      ))}
+    </div>
   );
 }
