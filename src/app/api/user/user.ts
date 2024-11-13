003fa1c49cd3f5183373de77a9fc4ab26631490a
dahhyeon <dahhyeon22@gmail.com>
2024-11-14 00:22:30 +0900

    chore:주석 제거

--- src/app/api/user/user.ts
+++ src/app/api/user/user.ts
@@ -70,7 +70,7 @@ export const getOtherUserInfo = async (
     }
 
     const data = await res.json();
-    console.log('프로필 조회 성공: ', data);
+    // console.log('프로필 조회 성공: ', data);
     return data.result;
   } catch (error) {
     console.error('Error fetching other user profile data: ', error);
