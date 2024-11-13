c6aae2fcefee0baa149c72644b476784a972736e
dahhyeon <dahhyeon22@gmail.com>
2024-11-14 00:17:42 +0900

    refactor:sidebar의 아이콘 다시 추가

--- src/app/(route)/(afterLogin)/_component/Sidebar.tsx
+++ src/app/(route)/(afterLogin)/_component/Sidebar.tsx
@@ -4,6 +4,9 @@ import { useEffect, useState } from 'react';
 import { useSelectedLayoutSegment } from 'next/navigation';
 import Link from 'next/link';
 import { getProfileInfo } from '@/app/api/user/user';
+import { AiFillHome } from 'react-icons/ai';
+import { RiMailLine } from 'react-icons/ri';
+import { CgProfile } from 'react-icons/cg';
 
 export default function SideBar() {
   const segment = useSelectedLayoutSegment();
@@ -13,9 +16,9 @@ export default function SideBar() {
     const fetchUserInfo = async () => {
       try {
         const userInfo = await getProfileInfo();
-        setMe({ id: userInfo.customId }); // customId로 설정
+        setMe({ id: userInfo.customId });
       } catch (error) {
-        console.error('Failed to fetch user info: ', error);
+        console.error('Failed to fetch user info:', error);
       }
     };
 
@@ -23,19 +26,37 @@ export default function SideBar() {
   }, []);
 
   const menuItems = [
-    { href: '/home', label: 'Home', segmentKey: 'home' },
-    { href: '/messages', label: 'Messages', segmentKey: 'messages' },
-    { href: `/${me?.id}`, label: 'Profile', segmentKey: me?.id },
+    {
+      href: '/home',
+      label: 'Home',
+      segmentKey: 'home',
+      icon: <AiFillHome className="w-7 h-7" />,
+    },
+    {
+      href: '/messages',
+      label: 'Messages',
+      segmentKey: 'messages',
+      icon: <RiMailLine className="w-7 h-7" />,
+    },
+    {
+      href: `/${me?.id}`,
+      label: 'Profile',
+      segmentKey: me?.id,
+      icon: <CgProfile className="w-7 h-7" />,
+    },
   ];
 
   return (
     <div className="space-y-4">
-      {menuItems.map(({ href, label, segmentKey }) => (
+      {menuItems.map(({ href, label, segmentKey, icon }) => (
         <div key={href}>
           <Link href={href}>
             <div
-              className={`flex items-center gap-4 text-xl px-4 py-3 rounded-full ${segment === segmentKey ? 'font-extrabold' : 'hover:bg-gray-800'}`}
+              className={`flex items-center gap-4 text-xl px-4 py-3 rounded-full ${
+                segment === segmentKey ? 'font-extrabold' : 'hover:bg-gray-800'
+              }`}
             >
+              {icon}
               <span>{label}</span>
             </div>
           </Link>
